610793335afa617b9bf33a81e27eaecbef36fb1c
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-11-14 14:34:11 +0000

    feat(partTwo):implement interface and its method

--- PartTwo_Calculator/src/Calculator.java
+++ PartTwo_Calculator/src/Calculator.java
@@ -1,2 +1,8 @@
-public class Calculator {
+import java.math.BigDecimal;
+
+public class Calculator implements RevenueCalculator {
+  @Override
+  public BigDecimal calculateRevenue(BigDecimal marginPercentage, BigDecimal costOfGoods) {
+    return null;
+  }
 }
