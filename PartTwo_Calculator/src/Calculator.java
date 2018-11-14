fe86b655cc7338a04cb378ad0e395e68bcfc92ee
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-11-14 14:35:32 +0000

    feat(partTwo):fields,constructor and properties

--- PartTwo_Calculator/src/Calculator.java
+++ PartTwo_Calculator/src/Calculator.java
@@ -1,8 +1,26 @@
 import java.math.BigDecimal;
 
 public class Calculator implements RevenueCalculator {
+  
+  private BigDecimal marginPercentage;
+  private BigDecimal costOfGoods;
+  
+  public Calculator(BigDecimal marginPercentage, BigDecimal costOfGoods) {
+    this.marginPercentage = marginPercentage;
+    this.costOfGoods = costOfGoods;
+  }
+  
+  public BigDecimal getMarginPercentage() {
+    return marginPercentage;
+  }
+  
+  public BigDecimal getCostOfGoods() {
+    return costOfGoods;
+  }
+  
   @Override
   public BigDecimal calculateRevenue(BigDecimal marginPercentage, BigDecimal costOfGoods) {
-    return null;
+  
   }
+  
 }
